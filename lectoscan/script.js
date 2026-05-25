5532873b8e8bddd3967d48b68b451375aefe647b
Dincy.com <diego972018@gmail.com>
2026-05-25 17:06:33 -0500

    Update script.js

--- lectoscan/script.js
+++ lectoscan/script.js
@@ -1,1 +1,23 @@
 
+console.log("LectoScan iniciado");
+
+// EFECTO SUAVE AL HACER SCROLL
+
+window.addEventListener("scroll", () => {
+
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+
+    const top = card.getBoundingClientRect().top;
+
+    if(top < window.innerHeight - 100){
+
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0px)";
+
+    }
+
+  });
+
+});
